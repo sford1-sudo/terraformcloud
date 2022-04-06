7494e9221d722cd25180e7fa33babf0d778234df
sford1-sudo <72464713+sford1-sudo@users.noreply.github.com>
2022-04-06 12:56:44 -0600

    Update test.tf

--- test.tf
+++ test.tf
@@ -55,7 +55,13 @@ resource "aws_security_group" "tf-sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   tags = {
     Name = "tf-sg"
   }
